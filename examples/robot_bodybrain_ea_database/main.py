5d1b2a3ae65769f9c69eb73fca1ec97ac5e7c5af
oliver weißl <oliver.weissl@outlook.com>
2024-02-09 09:00:17 +0100

    WIP

--- examples/robot_bodybrain_ea_database/main.py
+++ examples/robot_bodybrain_ea_database/main.py
@@ -1,6 +1,7 @@
 """Main script for the example."""
 
 import logging
+import time
 
 import config
 import multineat
@@ -16,16 +17,73 @@ from population import Population
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session
 
+from threading import Thread
+from queue import Queue
+
 from revolve2.experimentation.database import OpenMethod, open_database_sqlite
 from revolve2.experimentation.logging import setup_logging
 from revolve2.experimentation.optimization.ea import population_management, selection
 from revolve2.experimentation.rng import make_rng, seed_from_time
 
 
+class DatabaseHandler:
+    """A class to handle database commits."""
+    _commit_queue: Queue
+    _dbengine: Engine
+    _commit_thread: Thread
+
+    _is_active: bool
+    _terminating: bool
+
+    def __init__(self, dbengine: Engine) -> None:
+        """
+        Initialize the Handler.
+
+        :param dbengine: The database engine.
+        """
+        self._is_active = True
+        self._terminating = False
+        self._dbengine = dbengine
+        self._commit_queue = Queue()
+
+        self._commit_thread = Thread(target=self._commit)
+        self._commit_thread.start()
+
+    def _commit(self) -> None:
+        """Bulk save objects to database."""
+        while self._is_active:
+            if not self._commit_queue.empty():
+                logging.info("Saving element.")
+                with Session(self._dbengine, expire_on_commit=False) as session:
+                    session.add(self._commit_queue.get())
+                    session.commit()
+            if self._terminating:
+                self.disable()
+        else:
+            logging.info("Database operations concluded.")
+
+    def add_element(self, element: Base) -> None:
+        """
+        Add an element to the queue.
+
+        :param element: The element to add.
+        """
+
+        self._commit_queue.put(element)
+
+    def disable(self) -> None:
+        """Disable the Database Handler."""
+        if self._commit_queue.empty():
+            self._is_active = False
+        else:
+            self._terminating = True
+
+
+
 def select_parents(
-    rng: np.random.Generator,
-    population: Population,
-    offspring_size: int,
+        rng: np.random.Generator,
+        population: Population,
+        offspring_size: int,
 ) -> npt.NDArray[np.float_]:
     """
     Select pairs of parents using a tournament.
@@ -49,9 +107,9 @@ def select_parents(
 
 
 def select_survivors(
-    rng: np.random.Generator,
-    original_population: Population,
-    offspring_population: Population,
+        rng: np.random.Generator,
+        original_population: Population,
+        offspring_population: Population,
 ) -> Population:
     """
     Select survivors using a tournament.
@@ -76,24 +134,24 @@ def select_survivors(
 
     return Population(
         individuals=[
-            Individual(
-                genotype=original_population.individuals[i].genotype,
-                fitness=original_population.individuals[i].fitness,
-            )
-            for i in original_survivors
-        ]
-        + [
-            Individual(
-                genotype=offspring_population.individuals[i].genotype,
-                fitness=offspring_population.individuals[i].fitness,
-            )
-            for i in offspring_survivors
-        ]
+                        Individual(
+                            genotype=original_population.individuals[i].genotype,
+                            fitness=original_population.individuals[i].fitness,
+                        )
+                        for i in original_survivors
+                    ]
+                    + [
+                        Individual(
+                            genotype=offspring_population.individuals[i].genotype,
+                            fitness=offspring_population.individuals[i].fitness,
+                        )
+                        for i in offspring_survivors
+                    ]
     )
 
 
 def find_best_robot(
-    current_best: Individual | None, population: list[Individual]
+        current_best: Individual | None, population: list[Individual]
 ) -> Individual:
     """
     Return the best robot between the population and the current best individual.
@@ -108,12 +166,14 @@ def find_best_robot(
     )
 
 
-def run_experiment(dbengine: Engine) -> None:
+def run_experiment(database_handler) -> None:
     """
     Run an experiment.
 
     :param dbengine: An openened database with matching initialize database structure.
     """
+
+
     logging.info("----------------")
     logging.info("Start experiment")
 
@@ -123,10 +183,10 @@ def run_experiment(dbengine: Engine) -> None:
 
     # Create and save the experiment instance.
     experiment = Experiment(rng_seed=rng_seed)
-    logging.info("Saving experiment configuration.")
-    with Session(dbengine) as session:
-        session.add(experiment)
-        session.commit()
+    database_handler.add_element(experiment)
+    #with Session(dbengine, expire_on_commit=False) as session:
+    #    session.add(experiment)
+    #    session.commit()
 
     # Intialize the evaluator that will be used to evaluate robots.
     evaluator = Evaluator(headless=True, num_simulators=config.NUM_SIMULATORS)
@@ -164,12 +224,15 @@ def run_experiment(dbengine: Engine) -> None:
 
     # Finish the zeroth generation and save it to the database.
     generation = Generation(
-        experiment=experiment, generation_index=0, population=population
+        experiment=experiment,
+        generation_index=0,
+        population=population
     )
-    logging.info("Saving generation.")
-    with Session(dbengine, expire_on_commit=False) as session:
-        session.add(generation)
-        session.commit()
+
+    database_handler.add_element(generation)
+    #with Session(dbengine, expire_on_commit=False) as session:
+     #   session.add(generation)
+    #    session.commit()
 
     # Start the actual optimization process.
     logging.info("Start optimization process.")
@@ -215,10 +278,10 @@ def run_experiment(dbengine: Engine) -> None:
             generation_index=generation.generation_index + 1,
             population=population,
         )
-        logging.info("Saving generation.")
-        with Session(dbengine, expire_on_commit=False) as session:
-            session.add(generation)
-            session.commit()
+        #with Session(dbengine, expire_on_commit=False) as session:
+         #   session.add(generation)
+        #    session.commit()
+        database_handler.add_element(generation)
 
 
 def main() -> None:
@@ -233,9 +296,15 @@ def main() -> None:
     # Create the structure of the database.
     Base.metadata.create_all(dbengine)
 
+    database_handler = DatabaseHandler(dbengine)
+    times = []
     # Run the experiment several times.
     for _ in range(config.NUM_REPETITIONS):
-        run_experiment(dbengine)
+        start = time.time()
+        run_experiment(database_handler)
+        times.append(time.time() - start)
+    database_handler.disable()
+    print(f"AVG Time: {sum(times) / len(times)}")
 
 
 if __name__ == "__main__":
